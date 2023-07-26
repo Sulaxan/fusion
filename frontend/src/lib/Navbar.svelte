9bac1bfa6652f2233181853d9e69ca8079e6b72d
Sulaxan <sulaxan33@yahoo.ca>
2023-07-25 20:00:56 -0400

    reinitialize svelte and tauri projects, add navbar

---
+++ frontend/src/lib/Navbar.svelte
@@ -0,0 +1,16 @@
+<script>
+	import MiniProfile from "./profile/MiniProfile.svelte";
+
+</script>
+<div class="navbar bg-primary shadow-xl flex justify-between text-primary-content">
+	<div class="flex items-end">
+		<a class="btn btn-ghost normal-case text-xl" href="/">Fusion</a>
+		<p class="text-xs py-2">League Companion</p>
+	</div>
+	<input
+		type="text"
+		placeholder="Enter champion or player name..."
+		class="input bg-base-300 border-primary w-7/12 max-w-2xl"
+	/>
+    <MiniProfile />
+</div>
---
+++ frontend/src/lib/profile/MiniProfile.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { summonerName } from "$lib/stores";
+</script>
+
+<div class="flex gap-x-2">
+	<p class="hover:underline hover:underline-offset-2">{$summonerName}</p>
+	<img
+		src="https://raw.communitydragon.org/13.14/game/assets/ux/summonericons/profileicon549.png"
+        class="mask mask-circle w-12 h-auto"
+	/>
+</div>
